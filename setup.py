a5424e1fc59a1c0300394442c0047cbc60dce7db
ian <ian@nervos.org>
2019-03-27 09:45:28 +0800

    chore: setup long description for pip package

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import os
+import io
 from setuptools import setup
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+README = os.path.join(HERE, "README.md")
+with io.open(README, encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='cfbc',
       version='0.1',
       description='CFB code generator',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/nervosnetwork/cfb',
       author='Nervos Core Dev',
       author_email='dev@nervos.org',
